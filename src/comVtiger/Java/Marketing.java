d1ebeea4374388647b396ed6a2aa4e089a366441
Mangeshyadav123 <testermangesh7102@gmail.com>
2023-02-10 14:44:08 +0530

    my second user

---
+++ src/comVtiger/Java/Marketing.java
@@ -0,0 +1,10 @@
+package comVtiger.Java;
+
+public class Marketing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/comVtiger/Java/Practice.java
@@ -0,0 +1,11 @@
+package comVtiger.Java;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		String s = "Ram";
+		String s1="Shyam";
+
+	}
+
+}

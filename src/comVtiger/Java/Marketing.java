a8372a82c0dd2a7c886bf785116fd1ac910e9af8
Mangeshyadav123 <testermangesh7102@gmail.com>
2023-02-10 14:52:51 +0530

    my modified MarketingFile

--- src/comVtiger/Java/Marketing.java
+++ src/comVtiger/Java/Marketing.java
@@ -3,8 +3,8 @@ package comVtiger.Java;
 public class Marketing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello Marketing");
+		System.out.println("Marketing");
 	}
 
 }
